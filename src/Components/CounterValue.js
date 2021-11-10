57afdfae5bb7507fa0290b5d5e097de6e5400302
nvnsaharan <bb1180026@iitd.ac.in>
2021-11-10 23:36:38 +0530

    bdbch

--- src/Components/CounterValue.js
+++ src/Components/CounterValue.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./Counter.css";
 
 function CounterValue({ value }) {
-    return <h2>{value}</h2>;
+    return (
+        <div className="counter_value_div">
+            <h3>Counter Value</h3>
+            <h5>{value}</h5>
+        </div>
+    );
 }
 
 export default CounterValue;
